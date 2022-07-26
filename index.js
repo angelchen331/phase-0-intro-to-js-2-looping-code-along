cb7a4d2a6f20f8c5833b26820c2b61cb6c7c2f40
Yuchen Chen <angelchen3311@gmail.com>
2022-07-26 12:02:05 -0400

    commit tree bf03df351a0127e8d5d137bed51b71a8cfabd0aa

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = 'surprise'
+function writeCards(names, event){
+    const message  = [];
+    for (let i = 0; i < names.length; i++) {
+        message[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+return message;  
+}
+writeCards();
+
+function countDown(){
+    let countDown = 10;
+    while (countDown >=0){
+        console.log(countDown--);
+    }
+}
